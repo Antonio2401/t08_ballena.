2b2c5118f5f3986ea83a32151acd646121a72e67
Antonio2401 <57548440+Antonio2401@users.noreply.github.com>
2019-12-10 18:10:48 -0500

    Create ballena.py

---
+++ ballena.py
@@ -0,0 +1,6 @@
+            10        20        30       40
+#    01234567890123456789012345678901234567890
+msg="ESCUELA PROFESIONAL INGENERIA ELECTRONICA"
+print(msg[29:41]) ### muestra la cadena "ELECTRONICA"
+print(msg[8:14]) ### muestra la cadena "PROFES"
+print
